9f619181ceee492819c4d930d25c9fa4b0f05ce7
sarabuffalo <sep.14094@yahoo.com>
2018-05-08 12:21:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ function prependKitten(name) {
 }
 
 function removeLastKitten(name) {
-  var newKittens.slice(0, kittens.length - 1);
+  var newKittens = kittens.slice(0, kittens.length - 1);
   return newKittens;
 
 }
+
