c26179261eaf4e7d17cff8c2e5a4923d04481a73
sarabuffalo <sep.14094@yahoo.com>
2018-05-08 11:40:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const app = "I don't do much."
 
 function destructivelyAppendKitten(kittens) {
 var kittens = ["Milo", "Otis", "Garfield"];
-return kittens.push("Ralph");
+kittens.push("Ralph");
+return kittens;
 
 }
+
